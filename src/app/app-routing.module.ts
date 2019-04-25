3619f439cc1edd1eac219c2078bbd1ccdd88809b
Polina Chupailenko <pchup@softserveinc.com>
2019-04-25 17:18:10 +0300

    Added menu, routs, inputs and datepicker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
+import {TicketsComponent} from './containers/tickets/tickets.component';
+import {AddTicketComponent} from './containers/add-ticket/add-ticket.component';
+import {SearchComponent} from './containers/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: TicketsComponent},
+  { path: 'tickets', component: TicketsComponent },
+  { path: 'tickets/add', component: AddTicketComponent },
+  { path: 'search', component: SearchComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
+  // imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgbModule, NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicketsComponent } from './containers/tickets/tickets.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddTicketComponent } from './containers/add-ticket/add-ticket.component';
+import { SearchComponent } from './containers/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketsComponent,
+    HeaderComponent,
+    AddTicketComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
